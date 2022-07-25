54d05f7496644aa8c9d33598e228b9730ea3ad8b
AlanYu <alan0428a@gmail.com>
2022-07-25 13:52:48 +0800

    Update test_sample.py

--- test_sample.py
+++ test_sample.py
@@ -1,14 +1,16 @@
 from time import sleep
+from pytest_testrail.plugin import pytestrail
+
 
 def add(x):
     return x + 1
 
-
+@pytestrail.case('C2253')
 def test_should_pass():
     sleep(3)
     assert add(3) == 4
 
-
+@pytestrail.case('C2252')
 def test_should_fail():
     sleep(3)
     assert add(4) == 3
